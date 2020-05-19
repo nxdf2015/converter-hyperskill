1492928696282c7abf867a391c24f98ec9e39e04
xavier niquet <niquet_xavier2002@yahoo.fr>
2020-05-19 10:22:51 +0200

    stage 2

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
 package converter;
 
+import java.util.Scanner;
+
 public class Main {
+    public static int lastDigit(int n){
+        return n % 8 ;
+    }
     public static void main(String[] args) {
 
+        Scanner scanner =new Scanner(System.in);
+        int n = scanner.nextInt();
 
-        System.out.printf("%d  =  %s",10,"0b1010");
+        System.out.println(lastDigit(n));
     }
 }
